1e643a37fb0133b64a4bb39a654dbec63e620f2a
alex <alex@activewidgets.com>
2020-01-20 23:50:47 +0100

    use shared examples viewer

---
+++ examples/viewer/viewer.js
@@ -0,0 +1,32 @@
+﻿
+import {Viewer} from '@activewidgets/examples';
+import {render, h} from 'preact';
+import * as pages from '../index.js';
+import * as components from '../../';
+
+
+let tags = {};
+
+Object.keys(components).forEach(name => {
+    tags['ax-' + name.toLowerCase()] = components[name];
+});
+
+
+function mount(component, props){
+    let container = document.getElementById('app');
+    container.innerHTML = '';
+    render(h(tags[component], props), container);
+}
+
+
+function clean(){
+    let container = document.getElementById('app');
+    render(null, container);
+    container.innerHTML = '';
+}
+
+
+const viewer = new Viewer({
+    target: document.body,
+    props: {pages, mount, clean}
+});
